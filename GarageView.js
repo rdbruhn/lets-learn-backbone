6a41fdedb865d124c1589f221ed3d204bc20cbe2
Rob Bruhn <rdbruhn@gmail.com>
2016-03-24 12:00:17 -0500

    Redo view files for practice

--- GarageView.js
+++ GarageView.js
@@ -1,26 +1,20 @@
 var GarageView = Backbone.View.extend({
-  events: {
-    click: function() {
-      console.log('garageView was clicked', this);
-    }
-  },
-  initialize: function() {
-    this.render();
-    // better to use `listenTo` than `on` for event listeners
-    // read more here: http://backbonejs.org/#Events-listenTo
+  tagName: 'table',
+
+  template: _.template("<tr></tr>"),
+
+  initialize: function(){
     this.listenTo(this.collection, 'change', this.render);
+    this.render();
   },
-  render: function() {
-    var domElement = this.$el.html('');
-    var _this = this;
-    // get data from each model (which exists inside the collection)
-    this.collection.each(function(model) {
-      // with that data, we create some html
-      var carView = new CarView({ model: model }).render();
-      // we add that HTML to the body
-      domElement.append(carView);
+
+  render: function(){
+    this.$el.html('');
+    var context = this;
+    this.collection.each(function(car){
+      context.$el.append(new CarView({model: car}).render());
     });
     $('body').append(this.$el);
-  },
-  tagName: 'table'
+  },         
+
 });
